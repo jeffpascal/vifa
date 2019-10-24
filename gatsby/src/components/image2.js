34368706105e8d27828d1963d16c8b3f7eaf297e
Pascal Jean-Francois <jeffpascal96@gmail.com>
2019-10-24 17:54:44 +0300

    changed images

--- gatsby/src/components/image2.js
+++ gatsby/src/components/image2.js
@@ -34,17 +34,76 @@ function Image2() {
   const { node } = allFile.edges[index]
   return (
     <div>
-      <div className="fluid">
-        <Img
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <Img
+              fluid={node.childImageSharp.fluid}
+              key={node.id}
+              alt={node.name.replace(/-/g, " ").substring(2)}
+              className="d-block w-100"
+            />
+          </div>
+          {allFile.edges.map(image => (
+            <div className="carousel-item">
+              <Img
+                fluid={image.node.childImageSharp.fluid}
+                key={image.id}
+                alt={image.node.name.replace(/-/g, " ").substring(2)}
+                className="d-block w-100"
+              />
+            </div>
+          ))}
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+
+      <div>
+        {/* <Img
           fluid={node.childImageSharp.fluid}
           key={node.id}
           alt={node.name.replace(/-/g, " ").substring(2)}
-        />
-        
+        /> */}
       </div>
       <div>
-        <button onClick={() => handlePrevious()}>Previous</button>
-        <button onClick={() => handleNext()}>Next</button>
+        {/* <button onClick={() => handlePrevious()}>Previous</button>
+        <button onClick={() => handleNext()}>Next</button> */}
       </div>
     </div>
   )
