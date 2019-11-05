e874d2db5657a255f12d380eda96a923c009b852
Pascal Jean-Francois <jeffpascal96@gmail.com>
2019-11-05 11:42:25 +0200

    added fontawesome and formatted page listing

--- gatsby/src/components/listing2.js
+++ gatsby/src/components/listing2.js
@@ -1,14 +1,12 @@
 import React from "react"
 import SlickAll from "./slickgalleryall"
 import Slick from "./slickgallery"
-import "react-fontawesome"
-
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Listing2 = ({ listing, images }) => {
-
-  const camereImages = images.allFile.edges.filter(image => image.node.name.includes("camera"));
+  const camereImages = images.allFile.edges.filter(image =>
+    image.node.name.includes("camera")
+  )
 
   const getCamere = camere =>
     camere.map(camera => (
@@ -22,7 +20,11 @@ const Listing2 = ({ listing, images }) => {
 
   const getDotari = dotari =>
     dotari.map(dotare => (
-      <li className="list-group-item  flex-fill">
+      <li className="list-group-item w-50 list-group-item-action">
+        <i
+          class="far fa-check-square fa-lg"
+          style={{ color: "green", padding: 4}}
+        ></i>
         {dotare}
       </li>
     ))
@@ -31,49 +33,49 @@ const Listing2 = ({ listing, images }) => {
     descriere.map(descriere => (
       <div>
         {descriere.type === "normal" ? (
-          <p className="list-group-item-heading" style={{ color: "red" }}>{descriere.text}</p>
+          <p className="list-group-item-heading" style={{ color: "red" }}>
+            {descriere.text}
+          </p>
         ) : (
-            <p className="list-group-item-heading" style={{ color: "purple" }}>{descriere.text}</p>
-          )}
+          <p className="list-group-item-heading" style={{ color: "purple" }}>
+            {descriere.text}
+          </p>
+        )}
       </div>
     ))
-
 
   return (
     <div className="container">
       <h2>{listing.name}</h2>
 
-      
       <div className="row">
         <div className="col-md-6">
           <li className="list-group-item">
             <h3 className="list-group-item-heading">Descriere</h3>
             {//descriere
-              getDescriere(listing.detaliidescriere)}
+            getDescriere(listing.detaliidescriere)}
           </li>
         </div>
 
         <div className="col-md-6">
-        
-          
-            <h1>Camere</h1>
-            <ul className="list-group" >
+          <h1>Camere</h1>
+          <ul className="list-group">
             <Slick galleryImages={camereImages} />
-              {//camere
-                listing.camere &&
-                listing.camere.length > 0 &&
-                getCamere(listing.camere)}
-            </ul>
-          
-        </div>
-        <ul class="list-group">
-        </ul>
-        <div className="col-md-12 div_top_hypers">
-          <ul className="d-flex flex-wrap">
-            <h3 className="list-group-item-heading">Dotari</h3>
-            {//dotari
-              getDotari(listing.dotari)}
+            {//camere
+            listing.camere &&
+              listing.camere.length > 0 &&
+              getCamere(listing.camere)}
           </ul>
+        </div>
+        <ul class="list-group"></ul>
+        <div className="col-md-12">
+          <h3 className="list-group-item-heading">Dotari</h3>
+          <div className="list-group d-flex flex-row flex-wrap">
+            <ul className="d-flex flex-wrap">
+              {//dotari
+              getDotari(listing.dotari)}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
