8e73f0891cce054e7741fff8b41632939b050980
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-02-27 21:39:05 +0200

    fixed menu and added 2 extra pages

--- remade.vifa.ro/src/components/header.js
+++ remade.vifa.ro/src/components/header.js
@@ -7,47 +7,84 @@ const Header = ({ siteTitle }) => (
     style={{
       marginBottom: `1.45rem`,
     }}
-  > 
+  >
     <nav
-      className="navbar navbar-dark bg-dark"
+      className="navbar navbar-expand-lg navbar-dark bg-dark"
       style={{
         margin: `0 auto`,
-        padding: `1.45rem 1.0875rem`,
       }}
     >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <a className="navbar-brand" href="/">
         <Image></Image>
       </a>
-        <Link
-          className="btn btn-primary"
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-        <Link
-          className="btn btn-primary"
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Home
-        </Link>
-        <Link
-          className="btn btn-primary"
-          to="/contact"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Contact
-        </Link>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            <Link
+              className="btn btn-primary nav-link btn-lg"
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+                marginRight: `10px`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </li>
+          <li class="nav-item active">
+            <Link
+              className="btn btn-primary nav-link btn-lg"
+              to="/despre-noi"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+                marginRight: `10px`,
+              }}
+            >
+              Despre Noi
+            </Link>
+          </li>
+          <li class="nav-item active">
+            <Link
+              className="btn btn-primary nav-link btn-lg"
+              to="/imagini"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+                marginRight: `10px`,
+              }}
+            >
+              Imagini
+            </Link>
+          </li>
+          <li class="nav-item active">
+            <Link
+              className="btn btn-primary nav-link btn-lg"
+              to="/contact"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+                marginRight: `10px`,
+              }}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 )
---
+++ remade.vifa.ro/src/pages/imagini.js
@@ -0,0 +1,13 @@
+import React from "react"
+import SlickAll from "../components/slickgalleryall"
+import Layout from "../components/layout"
+
+const Imagini = () => {
+  return (
+    <Layout>
+      <SlickAll></SlickAll>
+    </Layout>
+  )
+}
+
+export default Imagini
