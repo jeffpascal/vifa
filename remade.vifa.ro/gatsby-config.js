cad459b0a5ef240472ffc6a66a1e67b80454366b
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-03-03 11:51:18 +0200

    Fixed contact and added recaptcha. Validated e-mail

--- remade.vifa.ro/gatsby-config.js
+++ remade.vifa.ro/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Vila Franceza`,
+    description: `Locuinte de inchiriat langa lacul colibita`,
+    author: `@jeffpascal`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- remade.vifa.ro/src/pages/contact.js
+++ remade.vifa.ro/src/pages/contact.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react"
+import React, { useState} from "react"
 import Layout from "../components/layout"
-
+import Reaptcha from 'reaptcha';
 const Contact = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [phoneNumer, setPhoneNumer] = useState("")
   const [description, setDescription] = useState("")
   const [confirmation, setConfirmation] = useState("")
+  const [captcha, setCaptcha] = useState(true)
   const [error, setError] = useState("No errors")
 
   function handleNameChange(event) {
@@ -26,14 +27,21 @@ const Contact = () => {
     console.log(event.target.value)
     console.log(event)
   }
+  function onVerify(token) {
+    setCaptcha(false)
+  }
 
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault()
-    const validateEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/
+    const validateEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
-    if (!validateEmail.test(email)) {
-      setError("E-mail must contain @")
+    console.log(email)
+    if ( !validateEmail.test(email)) {
+      setError("E-mail must contain @ and not be empty")
       return
+    }
+    if(captcha === true){
+      setError("Verify captcha")
     }
 
     const url = `https://iwk820vmz1.execute-api.eu-central-1.amazonaws.com/Prod/contact`
@@ -57,7 +65,9 @@ const Contact = () => {
         setError("")
         alert("success")
       } else {
-        setConfirmation("A fost o problema cu mesajul dumneavoastra. Va rog sa ne contactati direct la mailul unic_juridic@yahoo.com")
+        setConfirmation(
+          "A fost o problema cu mesajul dumneavoastra. Va rog sa ne contactati direct la mailul unic_juridic@yahoo.com"
+        )
         setError("")
       }
     })
@@ -65,98 +75,96 @@ const Contact = () => {
 
   return (
     <Layout>
-      <div className="col-md-6">
-        <form>
-          <div className="form-group">
-            <label htmlFor="inputmail">
-              Adresa de mail
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="inputmail"
-              aria-describedby="emailHelp"
-              placeholder="exemplu@gmail.com"
-              onChange={handleEmailChange}
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              Nu vom partaja mailul dumneavoastra cu nimeni
-            </small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="inputphone">
-              Numar de Telefon
-            </label>
-            <input
-              type="tel"
-              className="form-control"
-              id="inputphone"
-              placeholder="003371128829"
-              onChange={handlePhoneChange}
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              Includeti prefix exemplu: 0040755112151
-            </small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="inputname">
-              Nume intreg
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputname"
-              placeholder="Maria Silva Cosmina"
-              onChange={handleNameChange}
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              Nume + Prenume complet
-            </small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="inputcomment">
-              Comment:
-            </label>
-            <textarea
-              id="inputcomment"
-              className="form-control"
-              rows="5"
-              onChange={handleDescriptionChange}
-            ></textarea>
-            <small id="emailHelp" className="form-text">
+      <>
+        
+        <div className="col-md-6">
+          <form id="demo-form">
+            <div className="form-group">
+              <label htmlFor="inputmail">Adresa de mail</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputmail"
+                aria-describedby="emailHelp"
+                placeholder="exemplu@gmail.com"
+                onChange={handleEmailChange}
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                Nu vom partaja mailul dumneavoastra cu nimeni
+              </small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputphone">Numar de Telefon</label>
+              <input
+                type="tel"
+                className="form-control"
+                id="inputphone"
+                placeholder="003371128829"
+                onChange={handlePhoneChange}
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                Includeti prefix exemplu: 0040755112151
+              </small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputname">Nume intreg</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputname"
+                placeholder="Maria Silva Cosmina"
+                onChange={handleNameChange}
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                Nume + Prenume complet
+              </small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputcomment">Comment:</label>
+              <textarea
+                id="inputcomment"
+                className="form-control"
+                rows="5"
+                onChange={handleDescriptionChange}
+              ></textarea>
+              <small id="emailHelp" className="form-text">
+                Cu ce va putem ajuta
+              </small>
               <p style={{ color: "red" }}>{error}</p>
-              Cu ce va putem ajuta
-            </small>
-          </div>
-          <div className="form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label
-              htmlFor="exampleCheck1"
-              className="form-check-label"
-              defaultChecked={handleCheck}
-              onChange={handleCheck}
+            </div>
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label
+                htmlFor="exampleCheck1"
+                className="form-check-label"
+                defaultChecked={handleCheck}
+                onChange={handleCheck}
+              >
+                Sunt de acord cu prelucrarea datelor personale
+              </label>
+              <small id="exampleCheck1" className="form-text text-muted">
+                Nu vom partaja detaliile dumneavoastra cu nimeni altcineva.
+              </small>
+            </div>
+            <Reaptcha sitekey="6LccRd4UAAAAADH-A4ZbJFsDwWo_nVs4WIUwt-ld" onVerify={onVerify}  render="explicit" onloadCallback={() => console.log('loaded')}/>
+
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="btn btn-primary"
+              disabled={captcha}
             >
-              Sunt de acord cu prelucrarea datelor personale
-            </label>
-            <small id="exampleCheck1" className="form-text text-muted">
-              Nu vom partaja detaliile dumneavoastra cu nimeni altcineva.
-            </small>
-          </div>
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="btn btn-primary"
-          >
-            Submit
-          </button>
-          <p>{confirmation}</p>
-        </form>
-      </div>
-      <div></div>
+              Submit
+            </button>
+            <p>{confirmation}</p>
+          </form>
+        </div>
+        <div></div>
+      </>
     </Layout>
   )
 }
