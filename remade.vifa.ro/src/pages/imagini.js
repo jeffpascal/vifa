7f972f219cf47f78e37fc38b887e934b23e04d23
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-03-10 17:37:01 +0200

    Update imagini.js

--- remade.vifa.ro/src/pages/imagini.js
+++ remade.vifa.ro/src/pages/imagini.js
@@ -6,13 +6,25 @@ import { Link } from "gatsby"
 const Imagini = () => {
   return (
     <Layout>
-      <SEO title="Poze peisaj Colibita"
-      description="Poze peisaj"></SEO>
+      <SEO title="Poze peisaj Colibita" description="Poze peisaj"></SEO>
       <h1>Colibita in imagini</h1>
-      <p>Vila Franceza va ofera posibilitatea sa admirati lacul, muntii si padurile din imprejurimi chiar din interiorul ei sau de pe terasa generoasa dinspre lac.</p>
-      <p>Lacul Colibita are o frumusete aparte in fiecare moment al zilei. Va puteti face propriul album de imagini pe care sa le priviti cu placere in orice moment.</p>
-      <p>In orice sezon exista atractii care va vor face sa simtiti ca timpul trece mult prea repede.</p>
-      <p>Consultati <Link to="cazare-langa-lac">oferta noastra de cazare</Link> </p>
+      <p>
+        Vila Franceza va ofera posibilitatea sa admirati lacul, muntii si
+        padurile din imprejurimi chiar din interiorul ei sau de pe terasa
+        generoasa dinspre lac.
+      </p>
+      <p>
+        Lacul Colibita are o frumusete aparte in fiecare moment al zilei. Va
+        puteti face propriul album de imagini pe care sa le priviti cu placere
+        in orice moment.
+      </p>
+      <p>
+        In orice sezon exista atractii care va vor face sa simtiti ca timpul
+        trece mult prea repede.
+      </p>
+      <p>
+        Consultati <Link to="cazare-langa-lac">oferta noastra de cazare</Link>{" "}
+      </p>
       <SlickAll></SlickAll>
     </Layout>
   )
