af877e44f07d9155de635c04c7f09bbd00363b7d
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-06-20 23:40:15 +0300

    Update listingcard.js

--- remade.vifa.ro/src/components/listingcard.js
+++ remade.vifa.ro/src/components/listingcard.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Img from "gatsby-image"
 
-const ListingCard = props => {
+const ListingCard = (props) => {
   return (
-    <div className="card" style={{ width: 20 + "rem", "marginRight":20  }}>
+    <div className="card" style={{ width: 20 + "rem", marginRight: 20 }}>
       <Img className="card-img-top" fluid={props.image.childImageSharp.fluid} />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
-        <p className="card-text" dangerouslySetInnerHTML={{ __html: props.description }}></p>
+        <div
+          className="card-text"
+          dangerouslySetInnerHTML={{ __html: props.description }}
+        ></div>
         <a href="/cazare-langa-lac" className="btn btn-primary">
           {props.title}
         </a>
