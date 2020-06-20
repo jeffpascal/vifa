931fe4d7ebce0dc16ddc6e2a13643b4377582037
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-06-20 12:47:53 +0300

    added survey logic

--- remade.vifa.ro/src/pages/survey.js
+++ remade.vifa.ro/src/pages/survey.js
@@ -28,6 +28,110 @@ const SurveyPage = () => {
       }
     `
   )
+
+  const MainSurvey = (props) => {
+    let model = new Survey.Model(props.survey)
+    return <Survey.Survey model={model} onComplete={onComplete} />
+  }
+
+  const ImageSelector = () => {
+    return (
+      <div>
+        <div className="">
+          <div className="col-md-3">
+            <a onClick={setSurvey}>
+              <img
+                src={
+                  surveyImages.allFile.edges[0].node.childImageSharp.fluid.src
+                }
+              ></img>
+            </a>
+          </div>
+          <div className="col-md-3">
+            <a onClick={setSurvey}>
+              <img
+                src={
+                  surveyImages.allFile.edges[1].node.childImageSharp.fluid.src
+                }
+              ></img>
+            </a>
+          </div>
+          <div className="col-md-3">
+            <a onClick={setSurvey}>
+              <img
+                src={
+                  surveyImages.allFile.edges[2].node.childImageSharp.fluid.src
+                }
+              ></img>
+            </a>
+          </div>
+          <div className="col-md-3">
+            <a onClick={setSurvey}>
+              <img
+                src={
+                  surveyImages.allFile.edges[3].node.childImageSharp.fluid.src
+                }
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  // function useInput({ type /*...*/ }) {
+  //   const [value, setValue] = useState("")
+  //   const input = (
+  //     <input
+  //       value={value}
+  //       onChange={(e) => setValue(e.target.value)}
+  //       type={type}
+  //     />
+  //   )
+  //   return [value, input]
+  // }
+
+  // const [username, userInput] = useInput({ type: "text" })
+  // const [password, passwordInput] = useInput({ type: "text" })
+
+  // return (<>
+  //         {userInput} -> {username} <br />
+  //         {passwordInput} -> {password}
+  //       </>)
+
+  const InformationCollection = (props) => {
+    const [collectedEmail, setCollectedEmail] = useState(1)
+
+    function handleEmail(event) {
+      console.log(event.target.value)
+      setCollectedEmail(event.target.value)
+    }
+    console.log("what?")
+    if (number) {
+      return (
+        //number is in link
+        <form className="container jumbotron">
+          <div className="form-group">
+            <label htmlFor="inputmail">Adresa de mail</label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputmail"
+              aria-describedby="emailHelp"
+              placeholder="exemplu@gmail.com"
+              onChange={handleEmail}
+              aria-label="Enter e-mail address"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              Nu vom partaja mailul dumneavoastra cu nimeni
+            </small>
+          </div>
+        </form>
+      )
+    } //not in link
+    else return <p>a</p>
+  }
+
   function onComplete(survey, options) {
     //Write survey results into database
     console.log("Survey results: " + JSON.stringify(survey.data))
@@ -35,22 +139,15 @@ const SurveyPage = () => {
 
   function setSurvey(selectedSurvey) {
     if (selectedSurvey.target.getAttribute("src").includes("vilafranceza")) {
-      console.log("1")
       setChosenSurvey(quizVifa)
     }
     if (selectedSurvey.target.getAttribute("src").includes("apartamente")) {
-      console.log("2")
-
       setChosenSurvey(quizApartamente)
     }
     if (selectedSurvey.target.getAttribute("src").includes("casapescarului")) {
-      console.log("3")
-
       setChosenSurvey(quizCasapescarului)
     }
     if (selectedSurvey.target.getAttribute("src").includes("casagabriela")) {
-      console.log("4")
-
       setChosenSurvey(quizCasagabriela)
     }
 
@@ -293,54 +390,17 @@ const SurveyPage = () => {
       "<p>Your anwers are:</p><p>When was the Civil War?: <b>{civilwar}</b>. The correct is: <b>1850-1900</b></p><p>Who said 'Give me liberty or give me death?': <b>{libertyordeath}</b>. The correct is: <b>Patrick Henry</b></p><p>What is the Magna Carta?: <b>{magnacarta}</b>. The correct is: <b>The foundation of the British parliamentary system</b></p>",
   }
 
-  const [number, setNumber] = useState(null)
+  const [number, setNumber] = useState(
+    new URLSearchParams(window.location.search).get("telefon")
+  )
   const [chosenSurvey, setChosenSurvey] = useState(null)
-
-  useEffect(() => {
-    console.log(
-      "called useeffect" +
-        new URLSearchParams(window.location.search).get("telefon")
-    )
-    setNumber(new URLSearchParams(window.location.search).get("telefon"))
-  }, [])
-
-  let model = new Survey.Model(quizVifa)
+  const [wasInformationCollected, setWasInformationCollected] = useState(false)
 
   return (
-    <Layout>
-      <h1>La care casa ati fost, Apasati pe imagine pentru a face selectia</h1>
-      <div className="">
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={surveyImages.allFile.edges[0].node.childImageSharp.fluid.src}
-            ></img>
-          </a>
-        </div>
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={surveyImages.allFile.edges[1].node.childImageSharp.fluid.src}
-            ></img>
-          </a>
-        </div>
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={surveyImages.allFile.edges[2].node.childImageSharp.fluid.src}
-            ></img>
-          </a>
-        </div>
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={surveyImages.allFile.edges[3].node.childImageSharp.fluid.src}
-            ></img>
-          </a>
-        </div>
-      </div>
-      <p>{JSON.stringify(chosenSurvey)}</p>
-    </Layout>
+    <>
+      <InformationCollection></InformationCollection>
+      <MainSurvey survey={chosenSurvey}></MainSurvey>
+    </>
   )
 }
 
