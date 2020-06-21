b0208009071fc9e36aabd4e5e16ec90fa5e7292d
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-06-21 03:06:11 +0300

    generalized and resized images in image selector survey

--- remade.vifa.ro/src/components/Survey/ImageSelector/ImageSelector.js
+++ remade.vifa.ro/src/components/Survey/ImageSelector/ImageSelector.js
@@ -1,13 +1,14 @@
 import React from "react"
+import { useState } from "react"
 import "./ImageSelector.css"
+import Img from "gatsby-image"
 
 import { useStaticQuery, graphql } from "gatsby"
-
 
 const ImageSelector = (props) => {
   const surveyImages = useStaticQuery(
     graphql`
-      query surveyimages {
+      query surveyimagess {
         allFile(
           sort: { fields: name, order: DESC }
           filter: { relativeDirectory: { eq: "survey" } }
@@ -17,8 +18,8 @@ const ImageSelector = (props) => {
               id
               name
               childImageSharp {
-                fluid {
-                  src
+                fluid(maxWidth: 400, maxHeight: 400, quality: 90) {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
@@ -28,51 +29,55 @@ const ImageSelector = (props) => {
     `
   )
 
-  const {send} = props;
+  const { send } = props
 
-  function setSurvey(event){
-    send(event.target.src)
+  function setSurvey(event) {
+    send(event)
   }
+
+  function getProperTitle(title) {
+    if (title.includes("vilafranceza")) {
+      return "Vila Franceza"
+    }
+    if (title.includes("apartamente")) {
+      return "Apartamente moderne"
+    }
+    if (title.includes("casapescarului")) {
+      return "Casa Pescarului"
+    }
+    if (title.includes("casagabriela")) {
+      return "Casa Gabriela"
+    }
+  }
+
   return (
-    <div>
-      <div className="">
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={
-                surveyImages.allFile.edges[0].node.childImageSharp.fluid.src
-              }
-            ></img>
-          </a>
-        </div>
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={
-                surveyImages.allFile.edges[1].node.childImageSharp.fluid.src
-              }
-            ></img>
-          </a>
-        </div>
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={
-                surveyImages.allFile.edges[2].node.childImageSharp.fluid.src
-              }
-            ></img>
-          </a>
-        </div>
-        <div className="col-md-3">
-          <a onClick={setSurvey}>
-            <img
-              src={
-                surveyImages.allFile.edges[3].node.childImageSharp.fluid.src
-              }
-            ></img>
-          </a>
-        </div>
-      </div>
+    <div className="row container">
+      <h2 className="col-12">La ce casa ati fost?</h2>
+      {surveyImages.allFile.edges.map((property) => {
+        return (
+          <div
+            className="col-6"
+            onClick={() => {
+              setSurvey(property.node.childImageSharp.fluid.src)
+            }}
+            key={property.node.childImageSharp.fluid.src}
+          >
+            <div
+              style={{
+                height: "50px",
+                textAlign: "center",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <h5>{getProperTitle(property.node.childImageSharp.fluid.src)}</h5>
+            </div>
+            <a>
+              <Img fluid={property.node.childImageSharp.fluid} />
+            </a>
+          </div>
+        )
+      })}
     </div>
   )
 }
